9b964fa1fefd31ea22d4c11df8295b5615e0fe97
aleksey.a <alekseev.a@qasquad.ru>
2021-02-06 15:42:38 +0700

    update test project

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -4,13 +4,14 @@ import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class FirstTest {
+/*^^^подключаемые библиотеки^^^ программа сама подсказывает, когда надо поключать*/
 
+public class FirstTest {//объявление класса(имя класса должно совпадать с именем файла)
     public WebDriver driver;//объявление драйвера публично, чтобы пользоваться во всем классе
     public String baseUrl;//публичная переменная имени сайта
 
     @Before
-    public void before() {
+    public void before() {//метод, в котором происходит что то до старта тестов (аннотация @Before)
         System.setProperty("webdriver.chrome.driver", "chromedriver.exe");//путь к хромдрайверу(лежит в корне, поэтому просто название)
         driver = new ChromeDriver();//инициализируем драйвер
         baseUrl = "https://yandex.ru";//инициализация переменной имени сайта
@@ -18,12 +19,12 @@ public class FirstTest {
     }
 
     @Test
-    public void first() {
+    public void first() {//метод, в котором идет сам тест (аннотация @Test)
         driver.get(baseUrl);//команда get откроет сайт, который хранит переменная baseUrl
     }
 
     @After
-    public void quit() {
+    public void quit() {//метод, в котором происходит что то после отработки тестов (аннотация @After)
         driver.quit();//выключаем драйвер и закрываем все связанные с ним окна
     }
 }
