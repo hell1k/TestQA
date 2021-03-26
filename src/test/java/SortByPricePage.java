fca21b0497f2372cbe3374ba1ec41bc515a334cb
Konstantin <osipenko.k@qasquad.ru>
2021-03-26 17:14:58 +0700

    update

--- src/test/java/SortByPricePage.java
+++ src/test/java/SortByPricePage.java
@@ -21,9 +21,6 @@ public class SortByPricePage {
         PageFactory.initElements(driver, this);
     }
 
-    @FindBy(xpath = "//div[@class='catalog__item-price']")
-    WebElement goods;
-
     @FindBy(xpath = "//button[contains(text(), 'По новинкам') and contains(@class, 'item--active')]")
     WebElement novelty;
 
@@ -43,44 +40,51 @@ public class SortByPricePage {
         driver.get(url);
     }
 
+    /*
+     * Проверка сортировки цен по убыванию/возрастанию
+     *
+     */
     public void sortingByPrice(String increaseOrDecrease) {
-        int amount = Integer.parseInt(goodsAmount.getText().replaceAll("[^\\d.]", ""));
-        List<Integer> priceList = new ArrayList<Integer>();
+        novelty.click();//клик по меню сортировки
+        wait.until(ExpectedConditions.attributeContains(By.xpath("//div[@class='catalog__sort-list']/../self::div"), "class", "sort--open"));//проверка, что меню открылось
 
+        //клик по необходимой сортировке из меню, проверка, что загрузилась необходимая страница
+        if (increaseOrDecrease.equals("increase")) {
+            increaseInPrice.click();
+            Assert.assertTrue(catalogTitle.getText().contains("недорого"), "Несоответствие в заголовке страницы");
+        } else {
+            decreaseInPrice.click();
+            Assert.assertTrue(catalogTitle.getText().contains("премиум"), "Несоответствие в заголовке страницы");
+        }
+
+        //получаем количество карточек товара на странице
+        int amountCardsInPage = driver.findElements(By.xpath("//div[@class='catalog__item-price']")).size();
+
+        //список, в который будем собирать цены
+        List<Integer> priceList = new ArrayList<Integer>();
         String attribute;
-        String listPrice;
+        String price;
         int priceInt;
 
-        for (int i = 1; i <= amount; i++) {
+        //цикл для перебора карточек на странице, проверка есть ли у них скидка и добавление их в список
+        for (int i = 1; i <= amountCardsInPage; i++) {
             attribute = driver.findElement(By.xpath("(//div[@class='catalog__item-price'])[" + i + "]")).getAttribute("itemprop");
 
             if (attribute.equals("offers")) {
-                listPrice = driver.findElement(By.xpath("(//div[@class='catalog__item-price'])[" + i + "]//div[@class='catalog__item-price-new']")).getText().replaceAll("[^\\d.]", "");
+                price = driver.findElement(By.xpath("(//div[@class='catalog__item-price'])[" + i + "]//div[@class='catalog__item-price-new']")).getText().replaceAll("[^\\d.]", "");
             } else {
-                listPrice = driver.findElement(By.xpath("(//div[@class='catalog__item-price'])[" + i + "]")).getText().replaceAll("[^\\d.]", "");
+                price = driver.findElement(By.xpath("(//div[@class='catalog__item-price'])[" + i + "]")).getText().replaceAll("[^\\d.]", "");
             }
 
-            priceInt = Integer.parseInt(listPrice);
+            priceInt = Integer.parseInt(price);
             priceList.add(priceInt);
         }
 
-        int minPrice = Collections.min(priceList);
-        int maxPrice = Collections.max(priceList);
-
-        novelty.click();
-        wait.until(ExpectedConditions.attributeContains(By.xpath("//div[@class='catalog__sort-list']/../self::div"), "class", "sort--open"));
-
-        if (increaseOrDecrease.equals("increase")) {
-            increaseInPrice.click();
-            Assert.assertTrue(catalogTitle.getText().contains("недорого"), "Несоответствие");
-        } else {
-            decreaseInPrice.click();
-            Assert.assertTrue(catalogTitle.getText().contains("премиум"), "Несоответствие");
-        }
-
+        //получение атрибута первой и последней карточки для дальнейшей проверки есть ли у карточки скидка
         String firstAttribute = driver.findElement(By.xpath("(//div[@class='catalog__item-price'])[1]")).getAttribute("itemprop");
-        String lastAttribute = driver.findElement(By.xpath("(//div[@class='catalog__item-price'])[" + amount + "]")).getAttribute("itemprop");
+        String lastAttribute = driver.findElement(By.xpath("(//div[@class='catalog__item-price'])[" + amountCardsInPage + "]")).getAttribute("itemprop");
 
+        //получение цен первой и последней карточки
         int first;
 
         if (firstAttribute.equals("offers")) {
@@ -92,11 +96,16 @@ public class SortByPricePage {
         int last;
 
         if (lastAttribute.equals("offers")) {
-            last = Integer.parseInt(driver.findElement(By.xpath("(//div[@class='catalog__item-price'])[" + amount + "]//div[@class='catalog__item-price-new']")).getText().replaceAll("[^\\d.]", ""));
+            last = Integer.parseInt(driver.findElement(By.xpath("(//div[@class='catalog__item-price'])[" + amountCardsInPage + "]//div[@class='catalog__item-price-new']")).getText().replaceAll("[^\\d.]", ""));
         } else {
-            last = Integer.parseInt(driver.findElement(By.xpath("(//div[@class='catalog__item-price'])[" + amount + "]")).getText().replaceAll("[^\\d.]", ""));
+            last = Integer.parseInt(driver.findElement(By.xpath("(//div[@class='catalog__item-price'])[" + amountCardsInPage + "]")).getText().replaceAll("[^\\d.]", ""));
         }
 
+        //получение минимальной и максимальной цены из всего списка карточек на странице
+        int minPrice = Collections.min(priceList);
+        int maxPrice = Collections.max(priceList);
+
+        //сравнение мин/макс цены с первой и последней ценой карточки на странице
         if (increaseOrDecrease.equals("increase")) {
             Assert.assertEquals(first, minPrice);
             Assert.assertEquals(last, maxPrice);
